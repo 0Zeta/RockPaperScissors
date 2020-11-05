2296f92e0feab19dfff9499b3726c6af3ace3723
0Zeta <0Zeta@protonmail.com>
2020-11-06 00:25:12 +0100

    Implement naive frequency-based agent
    This agent plays (uniformly) randomly for the first 50 steps.
    After that it uses the relative frequencies of the opponent's previous actions as a probability distribution to chose it's own actions from. If this approach doesn't work well, we start playing randomly again.
    The decision whether we use frequencies or not is flipped with a certain probability.

---
+++ rpskaggle/agents/naive_agent.py
@@ -0,0 +1,55 @@
+from random import randint
+from rpskaggle.helpers import *
+
+
+class NaiveAgent(RPSAgent):
+    """
+    A simple agent that plays randomly for the first steps and uses the frequencies of the opponent's actions to
+    choose actions afterwards. When this doesn't work well, it switches to random play again.
+    """
+
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        self.play_random = True
+
+    def act(self) -> int:
+        if self.should_choose_random():
+            return randint(0, 2)
+
+        # Use the relative frequencies of the opponent's actions as a probability distribution for our actions
+        probs = counters(self.history['opponent_action']).value_counts(normalize=True, sort=False)
+        for i in range(SIGNS):
+            if i not in probs.keys():
+                probs.loc[i] = 0.
+        probs.sort_index(inplace=True)
+        return int(np.random.choice(range(SIGNS), p=probs))
+
+    def should_choose_random(self) -> bool:
+        # Play random for the first 50 steps
+        if self.step < 50:
+            self.play_random = True
+            return True
+        # and don't play random for the following 50 steps
+        elif self.step < 100:
+            self.play_random = False
+            return False
+
+        if not self.play_random:
+            if self.score < 30:
+                # Seems like our strategy doesn't work
+                self.play_random = True
+
+        # Flip the decision with a certain probability
+        return self.play_random if randint(1, 100) <= 85 else not self.play_random
+
+
+
+AGENT = None
+
+
+def naive_agent(observation, configuration) -> int:
+    global AGENT
+    if AGENT is None:
+        AGENT = NaiveAgent(configuration)
+    action, history = AGENT.agent(observation)
+    return action
---
+++ rpskaggle/helpers.py
@@ -0,0 +1,55 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+
+
+SIGNS = 3
+
+
+class RPSAgent(object):
+
+    def __init__(self, configuration):
+        self.obs = None
+        self.config = configuration
+        self.history = pd.DataFrame(columns=['step', 'action', 'opponent_action'])
+        self.history.set_index('step', inplace=True)
+
+        self.step = 0
+        self.score = 0
+
+    def agent(self, observation, configuration=None, history=None) -> Tuple[int, pd.DataFrame]:
+        if configuration is not None:
+            self.config = configuration
+        if history is not None:
+            self.history = history
+        self.obs = observation
+
+        self.step = self.obs.step
+
+        # Append the last action of the opponent to the history
+        if self.step > 0:
+            self.history.loc[self.step - 1, 'opponent_action'] = self.obs.lastOpponentAction
+            self.score = get_score(self.history)
+
+        # Choose an action and append it to the history
+        action = self.act()
+        self.history.loc[self.step] = {'action': action, 'opponent_action': None}
+        return action, self.history
+
+    def act(self) -> int:
+        pass
+
+
+def counters(actions: pd.Series) -> pd.Series:
+    """
+    Returns the counters for the specified actions
+    """
+    return (actions + 1) % SIGNS
+
+
+def get_score(history: pd.DataFrame) -> int:
+    score = 0
+    score += len(history[((history['opponent_action'] + 1) % SIGNS) == history['action']])
+    score -= len(history[((history['action'] + 1) % SIGNS) == history['opponent_action']])
+    return score
